43de1612b4f82eb13d31836dd6ffb6777b178488
afzalmuhammadhassan <muhammadhassanafzal014@gmail.com>
2020-11-11 18:39:29 +0900

    Edit main.py file.

--- HelloWorldApp/main.py
+++ HelloWorldApp/main.py
@@ -12,7 +12,11 @@ m3 = MITPerson('Omar Afzal')
 m4 = Person('Hasan Yaseen')
 m4.set_date_of_birth(2000,12,15)
 
-mit = [m2, m1, m3,m4]
+
+m5 = Person('Usman Yaseen')
+
+
+mit = [m2, m1, m3,m4, m5]
 mit.sort()
 for m in mit:
     print(m)
